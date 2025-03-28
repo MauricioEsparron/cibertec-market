45ad0d28935291346500b2b6b91ee655e57a351d
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-03-28 11:19:17 -0500

    Estructura base del proyecto con configuraciones basicas

---
+++ src/main/java/com/cibertec/cibertec_market/CibertecMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.cibertec_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibertecMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibertecMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/cibertec_market/CibertecMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.cibertec_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibertecMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
